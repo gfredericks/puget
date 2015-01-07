4087beb65258c0aa789015134dd5daa8afb73967
Gary Fredericks <fredericksgary@gmail.com>
2015-01-07 13:21:05 -0600

    Version 0.6.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks.forks.mvxcvi/puget "0.6.3.1-SNAPSHOT"
+(defproject com.gfredericks.forks.mvxcvi/puget "0.6.3.1"
   :description "Colorizing canonical Clojure printer for EDN values."
   :url "https://github.com/greglook/puget"
   :license {:name "Public Domain"
